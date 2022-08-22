949981af891752da1fbb0c6c833fe74584a90a8d
username <username@cognizant.com>
2022-08-22 21:05:59 +0530

    Add project files.

---
+++ prob 10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prob_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+            dic.Add(1, "Delhi");
+            dic.Add(2, "Mumbai");
+            dic.Add(3, "Chennai");
+
+            foreach (KeyValuePair<int, string> item in dic)
+            {
+                Console.WriteLine(item.Key + " " + item.Value);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
